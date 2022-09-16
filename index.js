cbea712aeac532303b9b994ac6cb0124c9a8a19e
quart-z <jtw2212@gmail.com>
2022-09-15 23:57:05 -0700

    Fixed import bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { getHTML, getTwitterFollowers } from './lib/scraper';
 
 async function go() {
-	getTwitterFollowers(await getHTML ('https://www.twitter.com/wesbos'));
+	getTwitterFollowers(await getHTML ('https://www.twitter.com/TheRock'));
 }
 
 go();
